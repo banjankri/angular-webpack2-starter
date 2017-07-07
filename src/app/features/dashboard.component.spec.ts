f7bc789717a5cb0abb6b1d5e70ecaacc7250e28a
Artur Banas <banas.artur@gmail.com>
2017-07-07 14:58:30 +0200

    Adding sinon to the stack

--- src/app/features/dashboard.component.spec.ts
+++ src/app/features/dashboard.component.spec.ts
@@ -1,4 +1,6 @@
 /* tslint:disable: max-line-length */
+import { ComponentFixtureAutoDetect } from '@angular/core/testing';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { TestBed, async } from '@angular/core/testing';
@@ -17,30 +19,42 @@ import { rootReducer } from '../reducers';
 import { UserActions } from './../user/user.actions';
 
 import 'rxjs/add/operator/takeUntil';
+import * as sinon from 'sinon';
 
 describe('Dashboard Component', () => {
-    beforeEach(() => {
+    let fixture,
+        userActionsStub;
+
+    beforeEach(async(() => {
+        userActionsStub = sinon.createStubInstance(UserActions);
+
         TestBed.configureTestingModule({
-        imports: [
-            BrowserTransferStateModule,
-            BrowserAnimationsModule,
-            MaterialModule,
-            ReactiveFormsModule,
-            HttpModule,
-            TransferHttpModule,
-            StoreModule.provideStore(rootReducer),
-        ],
-        providers: [
-            UserActions,
-        ],
-        declarations: [DashboardComponent]
+            imports: [
+                BrowserTransferStateModule,
+                BrowserAnimationsModule,
+                MaterialModule,
+                ReactiveFormsModule,
+                HttpModule,
+                TransferHttpModule,
+                StoreModule.provideStore(rootReducer, {
+
+                }),
+            ],
+            providers: [
+                { provide: UserActions, useValue: userActionsStub},
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ],
+            declarations: [DashboardComponent]
         });
-    });
+
+        fixture = TestBed.createComponent(DashboardComponent);
+    }));
 
     it('should contain welcome text', async(() => {
-        const fixture = TestBed.createComponent(DashboardComponent);
-        fixture.detectChanges();
         expect(fixture.nativeElement).toContainText('Welcome to the Dashboard');
     }));
 
+    it('should present users name in the input if present', async(() => {
+
+    }));
 });
