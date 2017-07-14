eddb602e0d5406aa64141ce070ba48e54fd10c77
Artur Banas <banas.artur@gmail.com>
2017-07-14 12:46:27 +0200

    Providers for activities registered

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,7 +1,11 @@
+import { ActivityService } from './activity/activity.service';
+import { ActivityActions } from './activity/activity.actions';
 import { UserActions } from './user/user.actions';
 import { UserService } from './user/user.service';
 
 export const APP_PROVIDERS = [
   UserActions,
-  UserService
+  UserService,
+  ActivityActions,
+  ActivityService
 ];
